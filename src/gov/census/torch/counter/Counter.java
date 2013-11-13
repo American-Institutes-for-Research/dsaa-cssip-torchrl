a310bc6e68426dff19b5a957ae696303def45bfe
Joshua Tokle <jtokle@gmail.com>
2013-11-13 11:13:29 -0500

    move counting and estimation into constructors

--- src/gov/census/torch/counter/Counter.java
+++ src/gov/census/torch/counter/Counter.java
@@ -3,17 +3,54 @@ package gov.census.torch.counter;
 import gov.census.torch.Record;
 import gov.census.torch.RecordComparator;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.TreeMap;
 
 public class Counter {
 
-    public Counter(RecordComparator comparator) {
-        this.comparator = comparator;
-        this._counts = new int[comparator.nPatterns()];
+    public Counter(RecordComparator cmp, Iterable<Record> list1, Iterable<Record> list2) {
+        _cmp = cmp;
+        _countMap = Counter.countPatterns(cmp, list1, list2);
+
+        // compact the counts into an array
+
+        _nonzeroCounts = new int[_countMap.size()];
+        _nonzeroPatterns = new int[_countMap.size()][cmp.nComparators()];
+        _nonzeroPatternIndex = new int[_countMap.size()];
+        int insertIndex = 0;
+
+        for (Integer ix: _countMap.keySet()) {
+            _nonzeroPatternIndex[insertIndex] = ix;
+            _nonzeroCounts[insertIndex] = _countMap.get(ix);
+
+            int[] pattern = cmp.patternFor(ix);
+            for (int k = 0; k < pattern.length; k++)
+                _nonzeroPatterns[insertIndex][k] = pattern[k];
+
+            insertIndex++;
+        }
     }
 
-    public void countPatterns(Iterable<Record> list1, Iterable<Record> list2) {
+    public RecordComparator recordComparator() {
+        return _cmp;
+    }
+
+    public int[] nonzeroCounts() {
+        return _nonzeroCounts;
+    }
+
+    public int[][] nonzeroPatterns() {
+        return _nonzeroPatterns;
+    }
+
+    protected static TreeMap<Integer, Integer> 
+        countPatterns(RecordComparator cmp, 
+                      Iterable<Record> list1, Iterable<Record> list2) 
+    {
+
+        TreeMap<Integer, Integer> countMap = new TreeMap<>();
 
         // Create an index on list1
 
@@ -40,17 +77,22 @@ public class Counter {
             } else {
                 LinkedList<Record> thisBlock = index.get(key);
                 for (Record otherRec: thisBlock) {
-                    int pattern = comparator.compareIndex(rec, otherRec);
-                    _counts[pattern]++;
+                    Integer pattern = cmp.compareIndex(rec, otherRec);
+                    if (countMap.containsKey(pattern)) {
+                        countMap.put(pattern, countMap.get(pattern) + 1);
+                    } else {
+                        countMap.put(pattern, 1);
+                    }
                 }
             }
         }
+
+        return countMap;
     }
 
-    public int[] counts() {
-        return _counts;
-    }
-
-    private RecordComparator comparator;
-    private int[] _counts;
+    private final RecordComparator _cmp;
+    private final TreeMap<Integer, Integer> _countMap;
+    private final int[] _nonzeroCounts;
+    private final int[][] _nonzeroPatterns;
+    private final int[] _nonzeroPatternIndex;
 }
--- src/gov/census/torch/model/ConditionalIndependenceModel.java
+++ src/gov/census/torch/model/ConditionalIndependenceModel.java
@@ -1,5 +1,6 @@
 package gov.census.torch.model;
 
+import gov.census.torch.counter.Counter;
 import gov.census.torch.RecordComparator;
 
 import java.util.Arrays;
@@ -10,22 +11,28 @@ public class ConditionalIndependenceModel {
     public final static double TOLERANCE = 0.0000001;
     public final static int MAX_ITER = 500;
 
-    public ConditionalIndependenceModel(RecordComparator cmp, int nClasses)
+    public ConditionalIndependenceModel(Random rng, Counter counter, int nClasses)
     {
         if (nClasses < 1)
             throw new IllegalArgumentException("'nClasses' must be greater than 0");
 
         _nClasses = nClasses;
-        this.cmp = cmp;
+        _cmp = counter.recordComparator();
 
-        _matchWeights = new double[nClasses][cmp.nComparators()][];
+        _matchWeights = new double[nClasses][_cmp.nComparators()][];
         for (int i = 0; i < nClasses; i++) {
-            for (int j = 0; j < cmp.nComparators(); j++) {
-                _matchWeights[i][j] = new double[cmp.nLevels(j)];
+            for (int j = 0; j < _cmp.nComparators(); j++) {
+                _matchWeights[i][j] = new double[_cmp.nLevels(j)];
             }
         }
 
         _classWeights = new double[nClasses];
+
+        estimate(rng, counter);
+    }
+
+    public ConditionalIndependenceModel(Counter counter, int nClasses) {
+        this(new Random(), counter, nClasses);
     }
 
     /**
@@ -54,7 +61,7 @@ public class ConditionalIndependenceModel {
         partitionOne(rng, _classWeights);
 
         for (int i = 0; i < _nClasses; i++) {
-            for (int j = 0; j < cmp.nComparators(); j++) {
+            for (int j = 0; j < _cmp.nComparators(); j++) {
                 partitionOne(rng, _matchWeights[i][j]);
             }
         }
@@ -81,58 +88,25 @@ public class ConditionalIndependenceModel {
         return _classWeights;
     }
 
-    public void estimate(int[] counts) {
-        this.estimate(new Random(), counts);
-    }
+    private void estimate(Random rng, Counter counter) {
 
-    public void estimate(Random rng, int[] counts) {
-
-        if (counts.length != cmp.nPatterns())
-            throw new IllegalArgumentException("counts.length != cmp.nPatterns()");
-
-        // count the number of nonzero patterns and create an index
-        int nNonzeroPatterns = 0;
-        for (int i = 0; i < counts.length; i++) {
-            if (counts[i] > 0)
-                nNonzeroPatterns++;
-        }
-
-        int[] nonzeroPatternIndex = new int[nNonzeroPatterns];
-        int[][] nonzeroPatterns = new int[nNonzeroPatterns][cmp.nComparators()];
-        int insertIndex = 0;
-
-        for (int i = 0; i < counts.length; i++) {
-            if (counts[i] > 0) {
-                nonzeroPatternIndex[insertIndex] = i;
-                int[] pattern = cmp.patternFor(i);
-
-                for (int j = 0; j < pattern.length; j++) {
-                    nonzeroPatterns[insertIndex][j] = pattern[j];
-                }
-                insertIndex++;
-            }
-        }
+        int[] counts = counter.nonzeroCounts();
+        int[][] patterns = counter.nonzeroPatterns();
 
         initWeights(rng);
 
-        double[][] expectedClass = new double[nNonzeroPatterns][_nClasses];
-        for (int i = 0; i < nNonzeroPatterns; i++)
+        double[][] expectedClass = new double[patterns.length][_nClasses];
+        for (int i = 0; i < patterns.length; i++)
             partitionOne(rng, expectedClass[i]);
 
-        double oldll = logLikelihood(nonzeroPatternIndex, 
-                                     nonzeroPatterns, 
-                                     counts, 
-                                     expectedClass);
+        double oldll = logLikelihood(counts, patterns, expectedClass);
         double newll, delta;
 
         for (int iter = 1; iter <= MAX_ITER; iter++) {
-            estep(nonzeroPatternIndex, nonzeroPatterns, expectedClass);
-            mstep(counts, nonzeroPatternIndex, nonzeroPatterns, expectedClass);
+            estep(patterns, expectedClass);
+            mstep(counts, patterns, expectedClass);
 
-            newll = logLikelihood(nonzeroPatternIndex, 
-                                  nonzeroPatterns, 
-                                  counts, 
-                                  expectedClass);
+            newll = logLikelihood(counts, patterns, expectedClass);
             delta = newll - oldll;
 
             if (iter % 10 == 0) {
@@ -153,13 +127,12 @@ public class ConditionalIndependenceModel {
         }
     }
 
-    private double logLikelihood(int[] nonzeroPatternIndex, int[][] nonzeroPatterns,
-                                 int[] counts, double[][] expectedClass) 
+    private double logLikelihood(int[] counts, int[][] patterns, double[][] expectedClass) 
     {
         double ll = 0.0;
-        int patternLength = cmp.nComparators();
+        int patternLength = _cmp.nComparators();
 
-        for (int i = 0; i < nonzeroPatternIndex.length; i++) {
+        for (int i = 0; i < patterns.length; i++) {
             double patll = 0.0;
 
             for (int j = 0; j < _nClasses; j++) {
@@ -167,32 +140,31 @@ public class ConditionalIndependenceModel {
                     continue;
 
                 for (int k = 0; k < patternLength; k++) {
-                    patll += Math.log(_matchWeights[j][k][nonzeroPatterns[i][k]]);
+                    patll += Math.log(_matchWeights[j][k][patterns[i][k]]);
                 }
 
                 patll += Math.log(_classWeights[j]);
                 patll *= expectedClass[i][j];
             }
 
-            ll += patll * counts[nonzeroPatternIndex[i]];
+            ll += patll * counts[i];
         }
 
         return ll;
     }
 
-    private void estep(int[] nonzeroPatternIndex, int[][] nonzeroPatterns, 
-                       double[][] expectedClass) 
+    private void estep(int[][] patterns, double[][] expectedClass) 
     {
-        int patternLength = cmp.nComparators();
+        int patternLength = _cmp.nComparators();
 
-        for (int i = 0; i < nonzeroPatternIndex.length; i++) {
+        for (int i = 0; i < patterns.length; i++) {
             double patternTotal = 0.0;
 
             for (int j = 0; j < _nClasses; j++) {
                 expectedClass[i][j] = _classWeights[j];
 
                 for (int k = 0; k < patternLength; k++) {
-                    expectedClass[i][j] *= _matchWeights[j][k][nonzeroPatterns[i][k]];
+                    expectedClass[i][j] *= _matchWeights[j][k][patterns[i][k]];
                 }
 
                 patternTotal += expectedClass[i][j];
@@ -203,13 +175,10 @@ public class ConditionalIndependenceModel {
         }
     }
 
-    private void mstep(int[] counts,
-                       int[] nonzeroPatternIndex, 
-                       int[][] nonzeroPatterns,
-                       double[][] expectedClass) 
+    private void mstep(int[] counts, int[][] patterns, double[][] expectedClass) 
     {
         for (int i = 0; i < _nClasses; i++) {
-            for (int j = 0; j < cmp.nComparators(); j++)
+            for (int j = 0; j < _cmp.nComparators(); j++)
                 Arrays.fill(_matchWeights[i][j], 0.0);
         }
 
@@ -217,17 +186,17 @@ public class ConditionalIndependenceModel {
         double countTotal = 0.0;
         for (int j = 0; j < _nClasses; j++) {
 
-            for (int u = 0; u < nonzeroPatterns.length; u++) {
-                double d = expectedClass[u][j] * counts[nonzeroPatternIndex[u]];
+            for (int u = 0; u < patterns.length; u++) {
+                double d = expectedClass[u][j] * counts[u];
                 classTotal[j] += d;
                 countTotal += d;
 
-                for (int k = 0; k < cmp.nComparators(); k++) {
-                    _matchWeights[j][k][nonzeroPatterns[u][k]] += d;
+                for (int k = 0; k < _cmp.nComparators(); k++) {
+                    _matchWeights[j][k][patterns[u][k]] += d;
                 }
             }
 
-            for (int k = 0; k < cmp.nComparators(); k++) {
+            for (int k = 0; k < _cmp.nComparators(); k++) {
                 for (int x = 0; x < _matchWeights[j][k].length; x++)
                     _matchWeights[j][k][x] /= classTotal[j];
             }
@@ -235,12 +204,10 @@ public class ConditionalIndependenceModel {
 
         for (int j = 0; j < _nClasses; j++)
             _classWeights[j] = classTotal[j] / countTotal;
-
-
     }
 
     private final int _nClasses;
-    private final RecordComparator cmp;
+    private final RecordComparator _cmp;
     private final double[][][] _matchWeights;
     private final double[] _classWeights;
 }
