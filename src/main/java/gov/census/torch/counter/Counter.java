9291d69be9836be8052a24ae1f40b470e438eada
Joshua Tokle <jtokle@gmail.com>
2013-11-19 09:44:16 -0500

    add tallyTruth method

--- src/main/java/gov/census/torch/counter/Counter.java
+++ src/main/java/gov/census/torch/counter/Counter.java
@@ -19,10 +19,41 @@ public class Counter {
     public static Tally tally(RecordComparator cmp, List<Record> list1, List<Record> list2) 
     {
 
-        TreeMap<Integer, Integer> countMap = new TreeMap<>();
+        IncrementalTally inc = new IncrementalTally(cmp);
         HashMap<String, List<Record>> blocks = Record.block(list1);
 
-        // Compare records in list2 to records in list1
+        for (Record rec: list2) {
+            String key = rec.blockingKey();
+
+            if (!blocks.containsKey(key)) {
+                continue;
+            } else {
+                List<Record> thisBlock = blocks.get(key);
+                for (Record otherRec: thisBlock)
+                    inc.add(rec, otherRec);
+            }
+        }
+
+        return inc.tally();
+    }
+
+    /**
+     * Counts truth patterns. This method uses blocking to bring together pairs, and so may not
+     * count every true match.
+     *
+     * @return a length 2 Tally array, in which the first element represents the observed patterns
+     * among match pairs, and the second element represents observed patterns amont nonmatch pairs.
+     */
+    public Tally[] tallyTruth(RecordComparator cmp, List<Record> list1, List<Record> list2) {
+        IncrementalTally trueMatch = new IncrementalTally(cmp);
+        IncrementalTally trueNonmatch = new IncrementalTally(cmp);
+
+        if (list1.size() > 0 && list2.size() > 0) {
+            if (!(list1.get(0).schema().hasId() && list2.get(0).schema().hasId()))
+                throw new IllegalArgumentException("Records must have ID fields");
+        }
+
+        HashMap<String, List<Record>> blocks = Record.block(list1);
 
         for (Record rec: list2) {
             String key = rec.blockingKey();
@@ -32,16 +63,14 @@ public class Counter {
             } else {
                 List<Record> thisBlock = blocks.get(key);
                 for (Record otherRec: thisBlock) {
-                    Integer pattern = cmp.compareIndex(rec, otherRec);
-                    if (countMap.containsKey(pattern)) {
-                        countMap.put(pattern, countMap.get(pattern) + 1);
-                    } else {
-                        countMap.put(pattern, 1);
-                    }
+                    if (rec.id().equals(otherRec.id()))
+                        trueMatch.add(rec, otherRec);
+                    else
+                        trueNonmatch.add(rec, otherRec);
                 }
             }
         }
 
-        return new Tally(cmp, countMap);
+        return new Tally[] {trueMatch.tally(), trueNonmatch.tally()};
     }
 }
